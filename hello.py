cb1d9ff564c356af2916a45a56760d309043e47e
Yayunyun <110958060+Yayunyun@users.noreply.github.com>
2022-09-13 23:19:53 +0000

    delete some  test files

--- hello.py
+++
@@ -1,6 +0,0 @@
-def add(x, y):
-    return x + y
-
-
-result = add(2, 3)
-print(f"this is the sum of {2} + {3}: {result}")
--- test_hello.py
+++
@@ -1,5 +0,0 @@
-from hello import add
-
-
-def test_add():
-    assert add(1, 2) == 3
