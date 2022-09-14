91c45c28bf3cf0a1feac9b8dcfd234e45b6f5d15
Yayunyun <110958060+Yayunyun@users.noreply.github.com>
2022-09-14 19:53:36 +0000

    add star

--- de_query_sql_db01.py
+++ de_query_sql_db01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from cmd import PROMPT
+from email.policy import default
 import click
 from dblib.querydb import querydb
 
@@ -11,13 +13,14 @@ def cli():
 
 # build a click command
 @cli.command()
-@click.option(
-    "--query",
-    default="SELECT * FROM default.diamonds LIMIT 2",
-    help="SQL query to execute",
-)
-def cli_query(query):
-    """Execute a SQL query"""
+@click.option("--age", default = 0, prompt="Age Above", help="Age above")
+@click.option("--gender", default = "1 | 2", prompt = "Gender(male = 1, female = 2")
+@click.option("--bmi", default = 0, prompt = "BMI above")
+@click.option("--rows", default = 3, prompt="Number of rows to return", help="Number of rows to return")
+
+def cli_query(age, gender, bmi, rows):
+    """Execute a SQL query on azureml diabetes open dataset"""
+    query = f"SELECT * FROM default.diabetes WHERE AGE > {age} AND SEX = {gender} AND BMI > {bmi} ORDER BY AGE  LIMIT {rows}"
     querydb(query)
 
 
